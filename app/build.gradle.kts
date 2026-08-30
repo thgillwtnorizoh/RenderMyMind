a829c32c30aeb768d38dc058b616a6012024c0eb
thgillwtnorizoh <81919671+thgillwtnorizoh@users.noreply.github.com>
2026-08-31 04:51:15 +0700

    Bump Alpha for native result OCR

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.example.rhythmtracker"
         minSdk = 29
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.3.2-alpha"
+        versionCode = 7
+        versionName = "0.3.3-alpha"
     }
 
     compileOptions {
